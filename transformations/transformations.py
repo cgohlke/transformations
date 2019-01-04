24a220e837284744cd15ad4e3161c8f525c9c2e7
Christoph Gohlke <cgohlke@uci.edu>
2019-01-04 10:26:41 -0800

    Update transformations/transformations.py

--- transformations/transformations.py
+++ transformations/transformations.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # transformations.py
 
-# Copyright (c) 2006-2018, Christoph Gohlke
-# Copyright (c) 2006-2018, The Regents of the University of California
+# Copyright (c) 2006-2019, Christoph Gohlke
+# Copyright (c) 2006-2019, The Regents of the University of California
 # Produced at the Laboratory for Fluorescence Dynamics
 # All rights reserved.
 #
@@ -47,12 +47,18 @@ functions to decompose transformation matrices.
 :Organization:
   Laboratory for Fluorescence Dynamics. University of California, Irvine
 
-:Version: 2018.10.18
+:Version: 2019.1.1
 
 Requirements
 ------------
 * `CPython 2.7 or 3.5+ <https://www.python.org>`_
 * `Numpy 1.14 <https://www.numpy.org>`_
+* A Python distutils compatible C compiler  (build)
+
+Revisions
+---------
+2019.1.1
+    Update copyright year.
 
 Notes
 -----
@@ -202,7 +208,7 @@ True
 
 from __future__ import division, print_function
 
-__version__ = '2018.10.18'
+__version__ = '2019.1.1'
 __docformat__ = 'restructuredtext en'
 
 import math
