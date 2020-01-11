df7a56061134d1c27ad0f92b8b5698bf04934ac0
Christoph Gohlke <cgohlke@uci.edu>
2020-01-10 16:30:26 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # transformations/setup.py
 
 """Transformations package setuptools script."""
@@ -18,11 +17,11 @@ version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
 
 description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
 
-readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from', code,
-                   re.MULTILINE | re.DOTALL).groups()[0]
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+                   code, re.MULTILINE | re.DOTALL).groups()[0]
 
-readme = '\n'.join([description, '=' * len(description)]
-                   + readme.splitlines()[1:])
+readme = '\n'.join([description, '=' * len(description)] +
+                   readme.splitlines()[1:])
 
 license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
                     re.MULTILINE | re.DOTALL).groups()[0]
@@ -31,6 +30,7 @@ license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
     with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
     with open('README.rst', 'w') as fh:
         fh.write(readme)
@@ -57,9 +57,9 @@ setup_args = dict(
     author='Christoph Gohlke',
     author_email='cgohlke@uci.edu',
     url='https://www.lfd.uci.edu/~gohlke/',
-    python_requires='>=2.7',
-    install_requires=['numpy>=1.11.3'],
-    setup_requires=['setuptools>=18.0', 'numpy>=1.11.3'],
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.14.5'],
+    setup_requires=['setuptools>=18.0', 'numpy>=1.14.5'],
     cmdclass={'build_ext': build_ext},
     packages=['transformations'],
     license='BSD',
@@ -72,14 +72,12 @@ setup_args = dict(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: C',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        ],
-    )
+        'Programming Language :: Python :: 3.8',
+    ],
+)
 
 try:
     if '--universal' in sys.argv:
