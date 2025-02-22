27de32ba5e74d43ce3910b32147770e215770c88
Christoph Gohlke <cgohlke@cgohlke.com>
2025-02-22 10:18:18 -0800

    Update transformations/__init__.py

--- transformations/__init__.py
+++ transformations/__init__.py
@@ -1,4 +1,17 @@
 # transformations/__init__.py
 
-from .transformations import __doc__, __version__
 from .transformations import *
+from .transformations import __all__, __doc__, __version__
+
+
+def _set_module() -> None:
+    """Set __module__ attribute for all public objects."""
+    globs = globals()
+    module = globs['__name__']
+    for item in __all__:
+        obj = globs[item]
+        if hasattr(obj, '__module__'):
+            obj.__module__ = module
+
+
+_set_module()
