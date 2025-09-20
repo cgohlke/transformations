2072e889dd9ef9d1f8872d36d5935b170db3f53e
Christoph Gohlke <cgohlke@cgohlke.com>
2025-09-20 10:46:09 -0700

    Update transformations/__init__.py

--- transformations/__init__.py
+++ transformations/__init__.py
@@ -3,15 +3,7 @@
 from .transformations import *
 from .transformations import __all__, __doc__, __version__
 
+# constants are repeated for documentation
 
-def _set_module() -> None:
-    """Set __module__ attribute for all public objects."""
-    globs = globals()
-    module = globs['__name__']
-    for item in __all__:
-        obj = globs[item]
-        if hasattr(obj, '__module__'):
-            obj.__module__ = module
-
-
-_set_module()
+__version__ = __version__
+"""Transformations version string."""
